ac9043acd0dd128280e7edb77fe408b955f84dcc
Tirapat <mind1990@gmail.com>
2018-11-08 13:40:14 -0800

    adds home.js

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+	render() {
+		return (
+			<div className='Search'>
+				<form>
+					<input type='text'></input>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default Search;
